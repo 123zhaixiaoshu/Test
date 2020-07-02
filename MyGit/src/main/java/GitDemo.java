d447bb8afe7c704dd2af56e704dbb16afd913fba
zyg <1739865737@qq.com>
2020-07-02 19:54:03 +0800

    GitDemo修改

--- MyGit/src/main/java/GitDemo.java
+++ MyGit/src/main/java/GitDemo.java
@@ -2,4 +2,10 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Git Test!!!");
     }
+
+    public void test(){
+        int num1 = 2;
+        int num3 = 4;
+        System.out.println(num1+num3);
+    }
 }
